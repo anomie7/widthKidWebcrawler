84084ae837e5e8f22303ff086f495900aa8915f0
Justin <anomie7777@gmail.com>
2018-10-28 16:03:59 +0900

    [Add] Controller 추가

---
+++ src/main/java/com/crawling/web/InterparkController.java
@@ -0,0 +1,19 @@
+package com.crawling.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.crawling.scheduler.InterParkCrawlingScheduler;
+
+@Controller
+public class InterparkController {
+	@Autowired
+	private InterParkCrawlingScheduler scheduler;
+	
+	@GetMapping(value="/")
+	public void findNewData() throws Exception {
+		scheduler.crawlingInterparkData();
+	}
+
+}
